323b7f8dce13412e35c3e8011dfdd0fe6e3b6908
Anil Pal <anilcodes01@gmail.com>
2025-09-12 14:07:54 +0530

    fixed alignment of explore button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,14 +12,14 @@ export default function Hero() {
         Empowering the Next Generation <br className="hidden md:block" />
         <span className="block">with Real-World Skills</span>
       </h1>
-      <p className="mt-4 md:mt-6 text-lg md:text-xl max-w-2xl opacity-90 mx-auto md:mx-0">
+      <p className="mt-4 md:mt-6 mb-12 text-lg md:text-xl max-w-2xl opacity-90 mx-auto md:mx-0">
         Experience hands-on learning with AI guidance, expert-curated
         projects, and career-ready outcomes
       </p>
       <Link
         // IMPORTANT: Allow clicks on the button
         href='/explore-courses'
-        className="pointer-events-auto relative px-8 py-3 mt-6 md:mt-8 text-lg font-semibold text-white rounded-xl border-none cursor-pointer bg-gradient-to-r from-orange-500 via-orange-400 to-yellow-400 bg-[length:300%_300%] animate-gradient-shift transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-orange-500/40 "
+        className="pointer-events-auto relative px-8 py-3 mt-16 md:mt-8 text-lg font-semibold text-white rounded-xl border-none cursor-pointer bg-gradient-to-r from-orange-500 via-orange-400 to-yellow-400 bg-[length:300%_300%] animate-gradient-shift transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-orange-500/40 "
       >
         Explore Courses
       </Link>

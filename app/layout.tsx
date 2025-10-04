ebf5f116d162cbafbd189ac00d36c2ced3bc2969
Anil Pal <anilcodes01@gmail.com>
2025-10-04 15:27:19 +0530

    added script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -11,7 +12,6 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Persevex",
   description: "Empowering careers",
- 
 };
 
 export default function RootLayout({
@@ -22,7 +22,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
-        {children} 
+        {/* Move the Script and noscript tags INSIDE the body tag */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '784980750633373');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=784980750633373&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {children}
       </body>
     </html>
   );
